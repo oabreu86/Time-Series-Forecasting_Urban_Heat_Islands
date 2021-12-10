9aab62bfec9f2e69dfa2732550ae14841877e213
Syeda Jaisha <syedajaisha@midway2-login1.rcc.local>
2021-12-10 15:25:23 -0600

    corrected parenthese errors

--- calc_features_func.py
+++ calc_features_func.py
@@ -10,11 +10,15 @@ def caclulate_features_from_landsat(b1, b2, b3, b4, b5, b6, b7, b10):
     ndvi = np.where((b4+b5)==0, 0, (b5-b4)/(b5+b4))
     ndsi =np.where((b3+b6)==0, 0, (b3-b6)/(b3+b6))
     ndbi = np.where((b6+b5)==0, 0, (b6-b5)/(b6+b5))
-    albedo = ((0.356*b1)+(0.1310*b2)+(0.373*b3)+\
-                    (0.085*b4)+(0.072*b5)-0.0018)/1.016
-    awei = 4*(b3-b6)-(0.25*b5 + 2.75*b6)
+
+    albedo = ((0.356*b2)+(0.1310*b4)+(0.373*b5)+\
+                    (0.085*b6)+(0.072*b7)-0.0018)/1.016
+
+    awei = 4*(b3-b6)-(0.25*b5) + (2.75*b6)
+
     eta = (2*(b5**2-b4**2) + 1.5*b5 + 0.5*b4) / (b5+b4+0.5)
     gemi = eta*(1-0.25*eta) - ((b4-0.125)/(1-b4))   
+
     min_ndvi = np.min(ndvi)
     max_ndvi = np.max(ndvi)
     min_ndvi_arr = np.full(shape=ndvi.shape, fill_value=float(min_ndvi))
@@ -22,7 +26,8 @@ def caclulate_features_from_landsat(b1, b2, b3, b4, b5, b6, b7, b10):
     if max_ndvi - min_ndvi == 0:
         prop_veg = np.full(shape=ndvi.shape, fill_value=0.0)
     else:
-        prop_veg =  (ndvi - min_ndvi_arr) / (max_ndvi_arr-min_ndvi_arr)**2
+        prop_veg =  ((ndvi - 0.2) / (0.3))**2
+        # prop_veg =  ((ndvi - min_ndvi_arr) / (max_ndvi_arr-min_ndvi_arr))**2
     LSE = (0.004 * prop_veg) + 0.986
     LST = (b10 / (1 + (10.895 * (b10/14380)) * (np.log(LSE))))
 
